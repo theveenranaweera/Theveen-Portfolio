a604f4469239040eb6b4732b4feefefc2f245138
Theveen Ranaweera <theveend@gmail.com>
2026-02-14 17:27:02 +0530

    chore: add github actions deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/", // ensures the site works on a custom domain root
   server: {
     host: "::",
     port: 8080,
